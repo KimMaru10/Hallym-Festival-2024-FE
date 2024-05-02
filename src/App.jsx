ba3f9a54ad575bbc3b0e11c8bb60d1a04224a1ec
김대성 <daesung3725@naver.com>
2024-05-03 02:38:37 +0900

    feat : 첫 커밋

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import NotFound from "./pages/NotFound/NotFound.jsx";
+import HallymFestival from "./pages/HallymFestival/HallymFestival.jsx";
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/Frist" element={<HallymFestival />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/HallymFestival/HallymFestival.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HallymFestival.scss";
+const HallymFestival = () => {
+  return (
+    <div className="hallymFestival">
+      <div>안녕</div>
+    </div>
+  );
+};
+
+export default HallymFestival;
