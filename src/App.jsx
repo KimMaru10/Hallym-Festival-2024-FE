90031535ef1e3c2385c1f45b54cf7f9dfc933727
Junseo11 <0302ser@naver.com>
2024-05-03 21:18:35 +0900

    design: 반응형 디자인 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import NotFound from "./pages/NotFound/NotFound.jsx";
 import HallymFestival from "./pages/HallymFestival/HallymFestival.jsx";
+import Info from "./pages/Info/Info.jsx";
 const App = () => {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" exact={true} element={<Home />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/hallymfestival" element={<HallymFestival />} />
+        <Route path="/info" element={<Info />} />
       </Routes>
     </div>
   );
