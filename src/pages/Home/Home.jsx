9aed5a7f7beb8a5336bf404375254793598cf5eb
김대성 <daesung3725@naver.com>
2024-05-03 13:04:41 +0900

    fix : Home입니다

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import "./Home.scss";
 const Home = () => {
-  return <div className="Home"></div>;
+  return <div className="Home">Home입니다.</div>;
 };
 
 export default Home;
