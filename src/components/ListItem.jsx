74458b4acaf85b4d1b1ab46eb0366ef16b751d74
Junseo11 <0302ser@naver.com>
2024-05-04 15:32:25 +0900

    Feat: 분실물 페이지 추가

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,23 +2,16 @@ import { useNavigate } from "react-router-dom";
 import "./Listitem.scss";
 
 const ListItem = ({title,albumId,url})=>{
-
-
-
     return(
-        <div className="ListItem">
-
-                    
-                <div className="item"> 
-                    <img src={url}/>
-                    <div className="text">
-                        <div id="text1">{albumId}</div>
-                        <div id="text2">{title}</div>
-                    </div>
+        <div className="ListItem">  
+            <div className="item"> 
+                <img src={url}/>
+                <div className="text">
+                    <div id="text1">{albumId}</div>
+                    <div id="text2">{title}</div>
+                </div>
 
             </div>
-
-
         </div>
     );
 };
--- src/pages/LostItem/LostItem.jsx
+++ src/pages/LostItem/LostItem.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 import Background from "../../components/Layout/Background";
+import Header from "../../components/Header";
+import axiosInstance from "../../apis/axiosInstance";
+import ListItem from "../../components/ListItem";
+import axios from "axios";
+import "./LostItem.scss";
+
+import { useEffect,useState } from "react";
 const LostItem = () => {
+    const [data,setData] = useState([]);
+
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/photos')
+        .then(response=>{
+          setData(response.data);
+      
+        });
+      },[]);
+
   return (
     <div className="lostItem">
-      <Background />
-      LostItem
+      <Header headcenter={"축제 분실물 찾기"}/>
+
+      <div className="list_wrapper">
+        {data.map((it)=>(
+          <div className="item_wrapper">
+              <img src={it.url}/>
+              <div className="text">
+                  <div id="text1">물품명: {it.albumId}</div>
+                <div id="text2">발견위치: {it.title}</div>
+
+          </div>
+          </div>
+          ))}
+      </div>
+
+
+
+
     </div>
   );
 };
