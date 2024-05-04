483767269d4e852eedc445598eca6b415b70b9dd
Gi_Hoon <kiki01111@naver.com>
2024-05-04 21:28:33 +0900

    Feat: 모달 슬라이드 구현

--- src/components/Modal/InfoModal.jsx
+++ src/components/Modal/InfoModal.jsx
@@ -1,22 +1,53 @@
-import React, { forwardRef } from "react";
+// Modal.js
+import React, { useState } from "react";
+import "./InfoModal.scss";
+import timetable from "../../datas/timetable.json";
+const InfoModal = ({ onClose }) => {
+  const dateArr = timetable.days.map((day) => day.date);
+  const imgArr = timetable.days.map((day) => day.photo_url);
+  const articleArr = timetable.days.map((day) => day.schedule);
+  console.log(articleArr);
+  const [pageDate, setPageDate] = useState(0);
 
-const InfoModal = forwardRef((props, ref) => {
   return (
-    <div ref={ref} {...props}>
-      <>
-        <div className="modalWrapper">
-          <h1>1일차</h1>
-          <div className="modalImage"></div>
-          <div className="modalArticle">
-            0900~1200 물총싸움<br></br> 1200~1400 밥
+    <div className="modalWrapper">
+      <div className="aboveModal"> 무대 타임 테이블</div>
+      <div className="modalBackground" onClick={(e) => e.stopPropagation()}>
+        <div>
+          <button onClick={onClose}>x</button>
+
+          <div className="showPageDate">{dateArr[pageDate]}</div>
+          <div className="slideAndImg">
+            <svg
+              className={`shift ${pageDate <= 0 ? "hidden" : ""}`}
+              onClick={() => {
+                setPageDate(pageDate - 1);
+              }}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+            >
+              <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+            </svg>
+            <div
+              className="modalImage"
+              style={{ backgroundImage: `url(${imgArr[pageDate]})` }}
+            ></div>
+            <svg
+              className={`shift ${pageDate > 3 ? "hidden" : ""}`}
+              onClick={() => {
+                setPageDate(pageDate + 1);
+              }}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+            >
+              <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+            </svg>
           </div>
+          <div className="modalArticle"></div>
         </div>
-      </>
+      </div>
     </div>
   );
-});
-
-// displayName 설정
-InfoModal.displayName = "InfoModal";
+};
 
 export default InfoModal;
--- src/pages/Info/Info.jsx
+++ src/pages/Info/Info.jsx
@@ -1,48 +1,21 @@
 import React, { useState } from "react";
 import "./Info.scss";
-import "./InfoModal.scss";
-
+import InfoModal from "../../components/Modal/InfoModal";
 const Info = () => {
-  const [boothModal, setBootModal] = useState(false);
+  const [boothModal, setBoothModal] = useState(false);
 
-  // 모달 백그라운드 및 Info 컴포넌트 클릭 시 모달 닫기
   const handleCloseModal = () => {
     if (boothModal) {
-      setBootModal(false);
+      setBoothModal(false);
     }
-  };
-
-  const handleModalContentClick = (e) => {
-    e.stopPropagation(); // 이벤트 버블링 중지
   };
 
   return (
     <div className="Info" onClick={handleCloseModal}>
       <div className="info_wrapper">
-        {boothModal && (
-          <div
-            className="modalBackground"
-            onClick={handleModalContentClick} // 모달 백그라운드 클릭 시 이벤트 전파 중지
-          >
-            <div className="modalWrapper">
-              <button
-                onClick={() => {
-                  setBootModal(false);
-                }}
-              >
-                x
-              </button>
-              <h1>1일차</h1>
-              <div className="modalImage"></div>
-              <div className="modalArticle">
-                0900~1200 물총싸움<br></br>1200~1400 밥
-              </div>
-            </div>
-          </div>
-        )}
         {!boothModal && (
           <>
-            <div className="info_box" onClick={() => setBootModal(true)}>
+            <div className="info_box" onClick={() => setBoothModal(true)}>
               부스 안내
             </div>
             <div className="info_box">무대 안내</div>
@@ -50,6 +23,7 @@ const Info = () => {
             <div className="info_box">공지사항</div>
           </>
         )}
+        {boothModal && <InfoModal onClose={() => setBoothModal(false)} />}
       </div>
     </div>
   );
