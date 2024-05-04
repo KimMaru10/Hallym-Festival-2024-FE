173b5758286f66074df8f87a94656c884b51a371
Gi_Hoon <kiki01111@naver.com>
2024-05-04 22:36:00 +0900

    Feat: 안내 페이지 모달 구현

--- src/components/Modal/InfoModal.jsx
+++ src/components/Modal/InfoModal.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import "./InfoModal.scss";
 import timetable from "../../datas/timetable.json";
-const InfoModal = ({ onClose }) => {
+const InfoModal = () => {
   const dateArr = timetable.days.map((day) => day.date);
   const imgArr = timetable.days.map((day) => day.photo_url);
   const articleArr = timetable.days.map((day) => day.schedule);
@@ -10,41 +10,42 @@ const InfoModal = ({ onClose }) => {
   const [pageDate, setPageDate] = useState(0);
 
   return (
-    <div className="modalWrapper">
-      <div className="aboveModal"> 무대 타임 테이블</div>
-      <div className="modalBackground" onClick={(e) => e.stopPropagation()}>
-        <div>
-          <button onClick={onClose}>x</button>
-
-          <div className="showPageDate">{dateArr[pageDate]}</div>
-          <div className="slideAndImg">
-            <svg
-              className={`shift ${pageDate <= 0 ? "hidden" : ""}`}
-              onClick={() => {
-                setPageDate(pageDate - 1);
-              }}
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 320 512"
-            >
-              <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-            </svg>
-            <div
-              className="modalImage"
-              style={{ backgroundImage: `url(${imgArr[pageDate]})` }}
-            ></div>
-            <svg
-              className={`shift ${pageDate > 3 ? "hidden" : ""}`}
-              onClick={() => {
-                setPageDate(pageDate + 1);
-              }}
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 320 512"
-            >
-              <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-            </svg>
-          </div>
-          <div className="modalArticle"></div>
+    <div className="modal">
+      {pageDate < 4 && <div className="modalAbove"> 무대 타임 테이블</div>}
+      <div className="modalBody" onClick={(e) => e.stopPropagation()}>
+        {/* <button onClick={onClose}>x</button> */}
+        <div className="modalTitle">{dateArr[pageDate]}</div>
+        <div className="modalImageSlider">
+          <svg
+            className={`shift ${pageDate <= 0 ? "hidden" : ""}`}
+            onClick={() => {
+              setPageDate(pageDate - 1);
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+          >
+            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+          </svg>
+          <div
+            className="sliderImage"
+            style={{ backgroundImage: `url(${imgArr[pageDate]})` }}
+          ></div>
+          <svg
+            className={`shift ${pageDate > 3 ? "hidden" : ""}`}
+            onClick={() => {
+              setPageDate(pageDate + 1);
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+          >
+            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+          </svg>
         </div>
+        <ul className="modalArticle">
+          {articleArr[pageDate].map((item, index) => (
+            <li key={index}>{`${item.time}  ${item.event}`}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
