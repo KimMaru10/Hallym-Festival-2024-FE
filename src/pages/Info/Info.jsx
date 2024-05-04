5db748006ad24ae9de77849cf676434341c41227
Gi_Hoon <kiki01111@naver.com>
2024-05-04 10:54:25 +0900

    Feat: 병합 전 커밋

--- src/pages/Info/Info.jsx
+++ src/pages/Info/Info.jsx
@@ -4,16 +4,12 @@ import "./Info.scss";
 const Info = () => {
   return (
     <div className="Info">
-
-        <div className="info_wrapper"> 
-            
-            <div className="info_box">부스 안내</div>
-            <div className="info_box">무대 안내</div>
-            <div className="info_box">기담 안내</div>
-            <div className="info_box">공지사항</div>
-        </div>
-
-      
+      <div className="info_wrapper">
+        <div className="info_box">부스 안내</div>
+        <div className="info_box">무대 안내</div>
+        <div className="info_box">기담 안내</div>
+        <div className="info_box">공지사항</div>
+      </div>
     </div>
   );
 };
