e7f3294ad6bba28e35beda095e8362c9931e7135
Junseo11 <0302ser@naver.com>
2024-05-04 11:55:04 +0900

    Feat: 헤더 추가

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.scss";
+
+const Header = ({headleft,headcenter,headright}) =>{
+    return(
+        <header className="header_wrapper">
+         <div className="haedleft">{headleft}</div>   
+         <div className="headcenter">{headcenter}</div>   
+         <div className="headright">{headright}</div>   
+        </header>
+    );
+};
+
+export default Header;
--- src/pages/PromotionalVideo/PromotionalVideo.jsx
+++ src/pages/PromotionalVideo/PromotionalVideo.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Background from "../../components/Layout/Background";
+import Header from "../../components/Header";
 const PromotionalVideo = () => {
   return (
     <div className="promotionalVideo">
-      <Background />
-      PromotionalVideo
+      <Header headcenter={"홍보영상"}/>
+
+
     </div>
   );
 };
