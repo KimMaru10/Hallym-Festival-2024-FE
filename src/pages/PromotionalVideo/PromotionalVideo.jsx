74c31baaeb8eec5bc6f6af8d1c2b88ab2a1de32a
Junseo11 <0302ser@naver.com>
2024-05-04 14:25:08 +0900

    Feat: 홍보영상 페이지 유튜브 연결

--- src/pages/PromotionalVideo/PromotionalVideo.jsx
+++ src/pages/PromotionalVideo/PromotionalVideo.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import Background from "../../components/Layout/Background";
 import Header from "../../components/Header";
+import YouTube from "react-youtube";
+import "./promotionalVideo.scss"
+
+
 const PromotionalVideo = () => {
+
+  const dummy = ["b1F2AVsJ05c","sMivc3RIrFs","b1F2AVsJ05c","b1F2AVsJ05c","b1F2AVsJ05c"];
+
+  const opts = {
+    height: '250',
+    width: '370',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  
   return (
     <div className="promotionalVideo">
       <Header headcenter={"홍보영상"}/>
+
+      <div className="item_wrapper">
+        {dummy.map((it)=>{
+          return(
+            <YouTube className="item" videoId={it}  opts={opts}  style={{ width: '100vw' }}/>
+          )
+        })}
+   
+      </div>
 
 
     </div>
