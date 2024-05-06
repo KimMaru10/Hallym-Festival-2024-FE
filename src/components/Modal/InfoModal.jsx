e410fb4600f13e44e018c1b42a2e6a8d76cc4612
Gi_Hoon <kiki01111@naver.com>
2024-05-07 00:20:25 +0900

    Fix: modal ui변경

--- src/components/Modal/InfoModal.jsx
+++ src/components/Modal/InfoModal.jsx
@@ -16,14 +16,13 @@ const InfoModal = ({ value }) => {
   // useEffect로 컴포넌트 마운트 시 애니메이션 시작
   useEffect(() => {
     const timer = setTimeout(() => {
-      setHeight("70vh"); // 0.3초 후에 높이를 70vh로 변경
+      setHeight("90vh"); // 0.3초 후에 높이를 70vh로 변경
     }, 100); // 약간의 지연을 주어 마운트 애니메이션을 보여줌
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <div className="modal">
-      {pageIndex < 4 && <div className="modalAbove">무대 타임 테이블</div>}
       <div
         className="modalBody"
         onClick={(e) => e.stopPropagation()}
@@ -31,7 +30,6 @@ const InfoModal = ({ value }) => {
       >
         <div className="modalTitle">{dateArr[pageIndex]}</div>
         <div className="modalImageSlider">
-          {/* 이전 페이지 이동 버튼 */}
           <svg
             className={`shift ${pageIndex <= 0 ? "hidden" : ""}`}
             onClick={() => setPageIndex(pageIndex - 1)}
@@ -40,12 +38,10 @@ const InfoModal = ({ value }) => {
           >
             <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
           </svg>
-          {/* 현재 이미지 */}
           <div
             className="sliderImage"
             style={{ backgroundImage: `url(${imgArr[pageIndex]})` }}
           ></div>
-          {/* 다음 페이지 이동 버튼 */}
           <svg
             className={`shift ${pageIndex > 3 ? "hidden" : ""}`}
             onClick={() => setPageIndex(pageIndex + 1)}
