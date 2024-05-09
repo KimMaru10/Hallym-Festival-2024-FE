132651cc9c971eb5080b9e5d360b8d65a53b8fdd
Junseo11 <0302ser@naver.com>
2024-05-09 16:53:29 +0900

    Feat: 예약 기능 추가

--- src/pages/Reservation/Reservation.jsx
+++ src/pages/Reservation/Reservation.jsx
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from "react";
 import Background from "../../components/Layout/Background";
-import ReservationWait from "./ReservationWaitModal";
+import ReservationWait from "../../components/Modal/ReservationWaitModal/ReservationWaitModal.jsx";
 import moment from "moment";
-import ReservationForm from "./ReservationForm";
+import ReservationForm from '../../components/ReservationForm/ReservationForm.jsx'
 import "./Reservation.scss"
+import { Header } from "../../components/index.js";
 
 const Reservation = () => {
 
   const [open, setOpen] = useState(false);
   const [nowTime,setNowTime] = useState(moment().format('HH:mm:ss'));
   useEffect(() => {
-    if(moment(nowTime, 'HH:mm:ss').isAfter(moment('13:15:00', 'HH:mm:ss'))) {
+    if(moment(nowTime, 'HH:mm:ss').isAfter(moment('18:00:00', 'HH:mm:ss'))) {
       setOpen(true);
-      console.log("참")
+    
     }
-    else{
+    else{ 
       setOpen(false);
-      console.log("거짓")
+
     }
   }, [nowTime]);
 
   return (
     <div className="reservation">
       <Background />
+      <Header />
+
+      <button onClick={()=>setOpen(true)}>폼 테스트</button>
+
+      
+
+      <header className="ReservationWaith2">주점예약</header>
 
       {open ? 
       <ReservationForm/>
