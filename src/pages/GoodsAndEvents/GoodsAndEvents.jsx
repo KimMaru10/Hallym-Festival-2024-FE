1eb18690b09647f240192a66b32483f8f0aeb81c
김대성 <daesung3725@naver.com>
2024-05-09 14:35:37 +0900

    feat : 굿즈/이벤트 스타일 추가

--- src/pages/GoodsAndEvents/GoodsAndEvents.jsx
+++ src/pages/GoodsAndEvents/GoodsAndEvents.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Background from "../../components/Layout/Background";
+import "./GoodsAndEvents.scss";
+import { Header } from "../../components/index.js";
 const GoodsAndEvents = () => {
   return (
     <div className="goodsAndEvents">
-      <Background />
-      GoodsAndEvents
+      <Background title="굿즈 / 이벤트" hasLogo={true} />
+      <Header />
+      <div className="goodsAndEvents-container">
+        <div className="goodsAndEvents-container-wrapper">
+          <div className="goodsAndEvents-container-wrapper-item">
+            <p>총학생회 '런'</p>
+          </div>
+          <div className="goodsAndEvents-container-wrapper-item">
+            <p>동아리연합회 '공성'</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
