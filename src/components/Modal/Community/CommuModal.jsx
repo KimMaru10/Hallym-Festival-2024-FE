76da77ff3065c9f988954acb65b2deff6a6aeb22
Gi_Hoon <kiki01111@naver.com>
2024-05-11 04:04:48 +0900

    Feat: 글자수 제한 및 글자수 세기 구현

--- src/components/Modal/Community/CommuModal.jsx
+++ src/components/Modal/Community/CommuModal.jsx
@@ -1,35 +1,48 @@
 import React, { useEffect, useState } from "react";
 import "./CommuModal.scss";
+
 const CommuModal = () => {
-  const [inputDate, setInputDate] = useState({
+  //등록 누르면 그 시점의 시간 보내는것도 구현
+  //삭제하기 구현
+  //등록하기 구현
+  const [inputData, setInputData] = useState({
     password: "",
     context: "",
   });
   const [pwErrorMsg, setPwErrorMsg] = useState("");
   const [contextMsg, setContextMsg] = useState("");
+  const [contextLen, setContextLen] = useState(0);
   useEffect(() => {
-    if (inputDate.password !== "") {
-      let pw = Number(inputDate.password);
+    if (inputData.password !== "") {
+      let pw = Number(inputData.password);
       isNaN(pw) ? setPwErrorMsg("숫자만 입력 가능합니다.") : setPwErrorMsg("");
     }
-  }, [inputDate.password]);
+  }, [inputData.password]);
   useEffect(() => {
     //욕설 리스트에서 가져옴...
     const forbiddenWordsRegex =
       /ㅅㅂ|ㅂㅅ|시발|병신|좆|ㅈ같|애미|노무현|이명박|섹스|sex|문제앙|새끼|가슴만|강간|개같은|개년|ㅂㅈ|따먹|부랄|자지|보지|유두|유방|창녀|창년|후장|fuck|bitch|penis|pennis|pussy/;
-    if (forbiddenWordsRegex.test(inputDate.context)) {
+    if (forbiddenWordsRegex.test(inputData.context)) {
       setContextMsg("욕설 및 부적절한 내용이 들어가 있습니다.");
     } else {
       setContextMsg(""); // 에러 메시지를 초기화합니다.
     }
-  }, [inputDate.context]);
+  }, [inputData.context]);
+
+  const onInputHandler = (e) => {
+    const inputText = e.target.value;
+    if (inputText.length <= 144) {
+      setInputData({ ...inputData, context: inputText });
+      setContextLen(inputText.length);
+    }
+  };
   return (
     <div className="modal">
       <div className="modal-body" onClick={(e) => e.stopPropagation()}>
         <div className="modal-body-title">
           <div className="modal-body-title-item"></div>
           <div className="modal-body-title-item">글쓰기</div>
-          <div className="modal-body-title-item closeBtn">닫기</div>
+          <div className="modal-body-title-item closeBtn">등록</div>
         </div>
         <div className="modal-body-article">
           <div className="modal-body-article-form">
@@ -38,24 +51,23 @@ const CommuModal = () => {
               <input
                 type="password"
                 placeholder="지우고 수정할 때 사용할 비밀번호 4자리입니다."
-                value={inputDate.password}
-                onChange={(e) => {
-                  setInputDate({ ...inputDate, password: e.target.value });
-                  //유효성 검사 추가하기
-                }}
+                value={inputData.password}
+                onChange={(e) =>
+                  setInputData({ ...inputData, password: e.target.value })
+                }
               />
               <div className="errMessage">{pwErrorMsg}</div>
             </div>
             <div className="modal-body-article-form-in">
               <label>남기고 싶은 말을 자세히 적어주세요.</label>
-              <input
+              <textarea
                 type="text"
+                maxLength="144"
                 placeholder="144이하로 작성해 주세요"
-                value={inputDate.context}
-                onChange={(e) =>
-                  setInputDate({ ...inputDate, context: e.target.value })
-                }
+                value={inputData.context}
+                onChange={onInputHandler}
               />
+              <div>{contextLen}/144자</div>
               <div className="errMessage">{contextMsg}</div>
             </div>
           </div>
