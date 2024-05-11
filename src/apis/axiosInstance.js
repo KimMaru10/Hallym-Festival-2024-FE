0999b23e341561b1490de58c95619ebd843c7177
Gi_Hoon <kiki01111@naver.com>
2024-05-11 22:00:06 +0900

    Feat: axios객체 수정

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,11 +1,13 @@
 import axios from "axios";
+import rateLimit from "axios-rate-limit";
 
-const axiosInstance = axios.create({
-  baseURL: "서버 URL", // 기본 URL 설정
+const createInstance = axios.create({
+  baseURL: "http://43.201.23.0", // 기본 URL 설정
   timeout: 10000, // 요청 타임아웃(ms)
-  // headers: {
-  //   "Content-Type": "multipart/form-data", // 요청 헤더 설정 중요하다 서버랑 맞춰라 임마!!!!
-  // },
 });
 
+const axiosInstance = rateLimit(createInstance, {
+  maxRequests: 10,
+  perMilliseconds: 1000,
+});
 export default axiosInstance;
