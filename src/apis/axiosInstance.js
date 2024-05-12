d64dad7ea933110893dc30c89311ecc4ccfeb752
김대성 <daesung3725@naver.com>
2024-05-13 01:41:25 +0900

    fix : axiosInstance 병합 충돌 해결

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 import rateLimit from "axios-rate-limit";
 
-<<<<<<< HEAD
-const axiosInstance = axios.create({
-=======
 const createInstance = axios.create({
->>>>>>> 18c7c16d2945352bccefc1db644e15bc9834de75
   baseURL: "http://43.201.23.0", // 기본 URL 설정
   timeout: 10000, // 요청 타임아웃(ms)
 });
