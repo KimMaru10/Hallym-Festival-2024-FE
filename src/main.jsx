204ca98dfd598d7e4b22979f01aac77267bca082
김대성 <daesung3725@naver.com>
2024-05-13 09:37:56 +0900

    test : image 미리 로딩

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./assets/fonts/font.css";
 import { BrowserRouter } from "react-router-dom";
+import PreloadImage from "./PreloadImage.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <PreloadImage src="./assets/logo1.png" />
       <App />
     </BrowserRouter>
   </React.StrictMode>
