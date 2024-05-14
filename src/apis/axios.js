41269d0e226a1ef0556d13b061f8ca01863da604
Gi_Hoon <kiki01111@naver.com>
2024-05-14 21:42:28 +0900

    Feat: 커뮤니티 삭제 url변경

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -3,7 +3,7 @@ import axiosInstance from "./axiosInstance";
 //http method담은 함수명은 http method와 유사하게 가져감, id가 포함된 메소드는 detail키워드 붙임
 export const getCommunity = async () => {
   try {
-    const response = await axiosInstance.get("/api/community");
+    const response = await axiosInstance.get("/community");
     console.log(response.data);
     return response;
   } catch (error) {
@@ -13,7 +13,7 @@ export const getCommunity = async () => {
 
 export const getCommunityDetail = async (id) => {
   try {
-    const response = await axiosInstance.get(`/api/community/${id}`);
+    const response = await axiosInstance.get(`/community/${id}`);
     return response;
   } catch (error) {
     console.error("커뮤니티 상세 불러오기 실패 : ", error);
@@ -22,7 +22,7 @@ export const getCommunityDetail = async (id) => {
 /**post의 성공여부를 boolean타입으로 반환 */
 export const postCommunity = async (data) => {
   try {
-    const response = await axiosInstance.post("/api/community", data);
+    const response = await axiosInstance.post("/community", data);
     console.log(response, "!!!!!!!!");
     if (response.status >= 200 && response.status < 300) {
       return true;
@@ -36,11 +36,12 @@ export const postCommunity = async (data) => {
     return false;
   }
 };
+/**혹시  */
 export const deleteCommunityDetail = async (id, password) => {
   try {
-    const response = await axiosInstance.delete(`/api/community/${id}`, {
-      data: { password: password },
-    });
+    const response = await axiosInstance.delete(
+      `/community/${id}?password=${password}`
+    );
     if (response.status >= 200 && response.status < 300) {
       return true;
     } else {
@@ -57,7 +58,7 @@ export const deleteCommunityDetail = async (id, password) => {
 //공지사항 불러오기
 export const getNoticeList = async () => {
   try {
-    const response = await axiosInstance.get("/api/notice");
+    const response = await axiosInstance.get("/notice");
     return response;
   } catch (error) {
     console.error("리스트 불러오기 실패 : ", error);
@@ -66,7 +67,7 @@ export const getNoticeList = async () => {
 
 export const getLostList = async () => {
   try {
-    const response = await axiosInstance.get("/api/find");
+    const response = await axiosInstance.get("/find");
     return response;
   } catch (error) {
     console.error("리스트 불러오기 실패 :", error);
