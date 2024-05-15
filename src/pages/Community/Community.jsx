0807bd5df6e4a284eafc9a96bd53516d6a65ff53
Gi_Hoon <kiki01111@naver.com>
2024-05-15 21:40:26 +0900

    Feat:뭐가 안되는건데

--- src/pages/Community/Community.jsx
+++ src/pages/Community/Community.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Background, Header } from "../../components/index.js";
 import CommuBoard from "../../components/CommunityComponents/CommuBoard.jsx";
-// import InfoModal from "../../components/Modal/InfoModal.jsx";
 import CommuModal from "../../components/Modal/Community/CommuModal.jsx";
 import "./Community.scss";
 
@@ -20,9 +19,6 @@ const Community = () => {
       <div className="community-container">
         <div className="community-container-wrapper">
           <div className="community-container-wrapper-notice">
-            {/* 해당 커뮤니티는 축제 관련 커뮤니티입니다. 타인을 비방, 조롱, 분란
-            조장, 운영 방해, 인식공격, 욕설, 비속어 등의 부적절한 내용은 법적
-            조치가 진행될 수 있습니다. */}
             <div>
               해당 커뮤니티는 축제 관련 커뮤니티입니다.<br></br>축제 커뮤니티에
               부적절한 내용은 <br></br>필터링 및 법적 조치가 진행될 수 있습니다.
--- src/pages/PubInfo/PubInfo.jsx
+++ src/pages/PubInfo/PubInfo.jsx
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./PubInfo.scss";
 import { Background, Header } from "../../components/index.js";
 
 const PubInfo = () => {
-  const [data, setData] = useState({});
+  // const [data, setData] = useState({});
 
-  useEffect(() => {
-    const dummyData = {
-      title: "Sample Title",
-      content:
-        "Sample Content Sample Content Sample Content Sample ContentSample Content Sample Content",
-    };
-    setData(dummyData);
-  }, []);
+  // useEffect(() => {
+  //   const dummyData = {
+  //     title: "운영시간 및 방침",
+  //     content:
+  //       "- 밤부스 운영시간 : 18시 ~ 02시예약자 입장 시간 : 17:30분 ~ 17:50분- 200 테이블 수용- 예약자는 100테이블 수용",
+
+  //     };
+  //   setData(dummyData);
+  // }, []);
 
   return (
     <div className="pubInfo">
@@ -22,8 +23,65 @@ const PubInfo = () => {
         <div className="pubInfo-container-wrapper">
           <img src={"https://via.placeholder.com/150"} alt="주점 위치" />
           <div className="contents">
-            <p id="title">{data.title}</p>
-            <p id="content">{data.content}</p>
+            <h1 className="title">운영시간 및 방침</h1>
+            <ul>
+              <li>
+                밤부스 운영시간 : 18시 ~ 02시<br></br>예약자 입자 시간 : 17:30 ~
+                17:50
+              </li>
+              <li>200 테이블 수용</li>
+              <li>예약자는 100테이블 수용</li>
+            </ul>
+            <h1 className="title">예약제 시스템</h1>
+
+            <ul>
+              <li>
+                웹사이트 주점 예약 창에서 각 주점 운영 전날 오전 8시30분 예약
+                <br></br>
+                ex&#41; 21일 테이블 예약 시 20일 오전 8시 30분 예약
+                <br></br>
+                (수업 시간을 고려한 시간대이며 다소 이른 시간이라도 양해
+                부탁드립니다)
+              </li>
+              <li>인당 1 테이블 예약 가능</li>
+              <li>
+                1 테이블 예약 시, 최대 4명까지 함께 예약 가능 (ex-의자 4개,
+                테이블 1개)
+              </li>
+              <li>대표자(예약자) 입장 필수!</li>
+              <li>
+                - 예약자 기준 야간주점 측에서 문자 발송 진행(입장 시 STAPP에게
+                보여준 후 입장)
+              </li>
+            </ul>
+
+            <h1 className="title">세부 사항</h1>
+            <li>수용 인원 : 100 테이블 수용</li>
+            <li>예약 마감 일정에 따라 조기 마감 될 수 있습니다</li>
+            <li>
+              입장 시간 : 17:30분 ~ 17:50분 입장(이 시간 내에 미방문 시 예약
+              취소)
+            </li>
+            <h1 className="title">밤부스 프로그램 부스 (ECO팝)</h1>
+            <ul>
+              <li>장소 : 지혜의길 (메인통제부스)</li>
+              <li>
+                방법 : 일정량의 쓰레기를 기준량보다 많이 모아오면
+                중앙통제부스에서 팝콘 배부
+              </li>
+              <h2>진행시간:</h2>
+              <li>20시~21시 </li>
+              <li>21시 30분~22시30분</li>
+              <li>23시 ~ 24시</li>
+            </ul>
+            <h1 className="title">판매 부스 (하리미네 잡화점) </h1>
+            <ul>
+              <li>숙취해소제 개당 1,500원</li>
+              <li>야광봉 2,000원</li>
+              <li>홍초 1,500원</li>
+              <li>깔라만시 2개 1,500원</li>
+              <li>새싹 삔 1개 500원</li>
+            </ul>
           </div>
         </div>
       </div>
