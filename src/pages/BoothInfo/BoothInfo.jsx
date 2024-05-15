59a3d02f3c3a3e55c186924b22aa668a11261936
Junseo11 <0302ser@naver.com>
2024-05-15 21:29:09 +0900

    Feat: 부스 리스트 추가

--- src/pages/BoothInfo/BoothInfo.jsx
+++ src/pages/BoothInfo/BoothInfo.jsx
@@ -1,52 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Board } from "../../components/index.js";
 import "./Boothinfo.scss";
+import boothList from "../../datas/booth.json"
 
 import { Header, Background } from "../../components/index.js";
 const BoothInfo = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    // 임시 데이터
-    const temporaryData = [
-      {
-        id: 1, // id 추가
-        url: "https://via.placeholder.com/150",
-        title: "부스 1", // title로 변경
-        content: "부스 1의 설명입니다.",
-      },
-      {
-        id: 2, // id 추가
-        url: "https://via.placeholder.com/150",
-        title: "부스 2", // title로 변경
-        content: "부스 2의 설명입니다.",
-      },
-      {
-        id: 3, // id 추가
-        url: "https://via.placeholder.com/150",
-        title: "부스 3", // title로 변경
-        content: "부스 3의 설명입니다.",
-      },
-      {
-        id: 1, // id 추가
-        url: "https://via.placeholder.com/150",
-        title: "부스 1", // title로 변경
-        content: "부스 1의 설명입니다.",
-      },
-      {
-        id: 2, // id 추가
-        url: "https://via.placeholder.com/150",
-        title: "부스 2", // title로 변경
-        content: "부스 2의 설명입니다.",
-      },
-      {
-        id: 3, // id 추가
-        url: "https://via.placeholder.com/150",
-        title: "부스 3", // title로 변경
-        content: "부스 3의 설명입니다.",
-      },
-    ];
-    setData(temporaryData);
+    setData(boothList);
   }, []);
 
   return (
