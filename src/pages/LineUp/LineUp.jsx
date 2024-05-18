e9c83dc8b5578b307d087ac7300a919c89c22b3e
김대성 <daesung3725@naver.com>
2024-05-18 14:19:15 +0900

    feat : 안쓰는 이미지 삭제 및 이미지 타입 변경

--- src/pages/LineUp/LineUp.jsx
+++ src/pages/LineUp/LineUp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Background, Header } from "../../components/index.js";
 import "./LineUp.scss";
-// import lineupImage from "../../assets/lineupImage.webp";
+import lineupImage from "../../assets/lineup.webp";
 const Lineup = () => {
   return (
     <div className="lineup">
@@ -9,10 +9,9 @@ const Lineup = () => {
       <Header className="header_blur" />
       <div className="lineup-class">
         <div className="lineup-class-lineup">
-          <img
-            src="https://sunwoo-img-container.s3.ap-northeast-2.amazonaws.com/front/d4fe424f-214a-44df-b018-7fff0c7874a0_lineuplist.svg"
-            alt="라인업 이미지"
-          />
+          <div className="lineup-class-lineup-wrapper">
+            <img src={lineupImage} alt="라인업 이미지" />
+          </div>
         </div>
       </div>
     </div>
