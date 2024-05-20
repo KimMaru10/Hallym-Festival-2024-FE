2ad9cdd7f2fb094a7359fe9fceebbe859ee4b729
Junseo11 <0302ser@naver.com>
2024-05-20 17:37:47 +0900

    Fix: 예약 ui 개선

--- src/components/ReservationInfoComponent/ReservationInfoComponent.jsx
+++ src/components/ReservationInfoComponent/ReservationInfoComponent.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 const ReservationInfoComponent = ({data,isRes})=>{
     const navigate = useNavigate();
 
+
+
     return(
         <div className="ReservationInfoComponent">
             <h3 className="res_info_wrapper">예약 확인 결과 </h3>
@@ -12,11 +14,11 @@ const ReservationInfoComponent = ({data,isRes})=>{
             <p className="res_info_p">
                 <b>인원수 {data.people_count}명 </b>
                 <br />
-                <b>학번: {data.studentId} </b>
+                <b>학번: {data.studentNum} </b>
                 <br />
-                <b>이름: {data.name} </b>
+                <b>이름: {data.studentName} </b>
                 <br />
-                <b>전화번호: {data.phone_number} </b>
+                <b>전화번호: {`0${data.phone_number}`} </b>
                 <br />
                 <br />
                 <p>예약 성공이 확인되었습니다.</p>  
